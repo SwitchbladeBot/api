43888cc89ddda4d1436f5ccc0b0ca6eb53ec8edf
pedrofracassi <pedroluizfracassi@gmail.com>
2020-04-15 22:33:52 +0000

    Add sentry error collection

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const Sentry = require('@sentry/node')
+
+Sentry.init({ dsn: process.env.SENTRY_DSN })
 
 const app = express()
 
 const PORT = process.env.PORT || 80
 
-// Routes
-const contributorsRoute = require('./routes/contributors')
+app.use(Sentry.Handlers.requestHandler())
 
-// TODO: configure cors
+// TODO: Set up CORS
 app.use(cors())
 
+const contributorsRoute = require('./routes/contributors')
 app.use(contributorsRoute())
-// TODO: morgan log
+// TODO: Add morgan logging
 
 app.get('/', (req, res) => {
   res.json({ message: "Hello World" })
 })
+
+app.use(Sentry.Handlers.errorHandler())
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
